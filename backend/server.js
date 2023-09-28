32309ef0218b0ca48ee2b621e4648ac9972fef89
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-28 17:18:01 +0300

    Fix errors

--- backend/server.js
+++ backend/server.js
@@ -21,18 +21,10 @@ if (
   process.env.NODE_ENV === 'production' ||
   process.env.NODE_ENV === 'staging'
 ) {
-  app.use(express.static('client/build'));
+  app.use('/', express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + './client/build/index.html'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
-}
-
-// Heroku production setup
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
-  );
 }
 
 // if (process.env.NODE_ENV === 'production') {
