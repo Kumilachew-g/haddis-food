4bdd5ea30eb7a1b1122105e7185bced9354763de
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-28 17:27:13 +0300

    fix

--- backend/server.js
+++ backend/server.js
@@ -16,24 +16,13 @@ app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-// Vercel production setup
-if (
-  process.env.NODE_ENV === 'production' ||
-  process.env.NODE_ENV === 'staging'
-) {
+if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static('client/build'));
+
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
   });
 }
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use('/', express.static('client/build'));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
-//   });
-// }
 
 const port = process.env.PORT || 5000;
 
