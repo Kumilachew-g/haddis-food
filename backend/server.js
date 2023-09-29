59c6666224a3bf6ad131c0efa2036dad7ea5a344
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-29 13:03:22 +0300

    Add more deployment route

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,33 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
+  app.get('/login', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  app.get('/signup', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  app.get('/cart', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  app.get('/checkout', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  app.get('/admin', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  app.get('/order', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  app.get('/order/:id', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  app.get('/order/:id/edit', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+  app.get('/order/:id/delete', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
 
 const port = process.env.PORT || 5000;
