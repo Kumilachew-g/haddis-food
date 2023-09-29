0a77afe4e91ccf57193f0a0cd8d8bc6f68ca3e2f
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-29 16:58:44 +0300

    Fix server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 
 const food = require('./models/foodModel');
 
@@ -15,6 +16,14 @@ const orderRouter = require('./routes/ordersRoute');
 app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './client/build/index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
